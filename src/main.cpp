389f5644844a958b060415c1468b70394128710f
Miunn <remcaulier@orange.fr>
2024-02-06 10:35:03 +0100

    First move game PoC

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include "../includes/Board.hpp"
 
+#include <chrono>
+
 using namespace std;
 
 int main(int argc, char *argv[]) {
@@ -9,6 +11,45 @@ int main(int argc, char *argv[]) {
 
     cout << b << endl;
 
+    auto t1 = std::chrono::high_resolution_clock::now();
+    vector<string> moves = b.getValidMove(Pawn::BLACK);
+    auto t2 = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1);
+
+    for (int i = 0; i < (int)moves.size(); i++) {
+        cout << moves[i] << " ";
+    }
+    cout << endl;
+
+    cout << "Exec time: " << duration.count() << " microseconds" << endl;
+
+    vector<string> history = {};
+    Pawn currentPlayer = Pawn::BLACK;
+    for (vector<string> moves = b.getValidMove(currentPlayer); (int)moves.size() > 0; moves = b.getValidMove(currentPlayer)) {
+        cout << b << endl;
+
+        cout << "Play: " << moves[0] << endl;
+        
+        auto t1 = std::chrono::high_resolution_clock::now();
+        if (!b.play(currentPlayer, moves[0])) {
+            cout << "Play returned false" << endl;
+            break;
+        }
+        auto t2 = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1);
+        cout << "Time to play " << duration.count() << " microseconds" << endl;
+
+        history.push_back(moves[0]);
+        if (currentPlayer == Pawn::BLACK) currentPlayer = Pawn::WHITE;
+        else currentPlayer = Pawn::BLACK;
+    }
+
+    cout << b << endl << "Ended game, player " << currentPlayer << endl;
+    cout << "History: ";
+    for (int i = 0; i < (int)history.size(); i++) {
+        cout << history[i] << " ";
+    }
+    cout << endl;
 
     return 0;
 }
