f67d3a94d18f40d8979b81fcc89c99e00cda0bbb
Miunn <remcaulier@orange.fr>
2024-02-13 13:57:54 +0100

    AI interface docstrings

--- includes/AInterface.hpp
+++ includes/AInterface.hpp
@@ -3,14 +3,38 @@
 #include <iostream>
 #include "Board.hpp"
 
+/**
+ * @brief Interface for the differents AI algorithms of the project
+ * 
+ */
 class AInterface
 {
     public:
+    /**
+     * @brief Play a move on the board
+     * 
+     * @param board Copy of the board, its not the AI who modify the value, just research on it
+     * @return std::string Coordinate to play
+     */
     virtual std::string play(const Board& board) const = 0;
+
+    /**
+     * @brief Display associated pay off matrix
+     * 
+     */
     void showScores() const;
 
     protected:
+    /**
+     * @brief Player played by the AI
+     * 
+     */
     Pawn player;
+
+    /**
+     * @brief Pay off matrix
+     * 
+     */
     int scores[64] = {
         500, -150, 30, 10, 10, 30, -150, 500,
         -150, -250, 0, 0, 0, 0, -250, -150,
