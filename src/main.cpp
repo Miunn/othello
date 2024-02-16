619328667b9c8dbc129098d299b753bf4326e46d
Miunn <remcaulier@orange.fr>
2024-02-16 10:03:24 +0100

    AI cli

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <chrono>
+#include <string>
 #include "../includes/Board.hpp"
 #include "../includes/Game.hpp"
 #include "../includes/Player.hpp"
@@ -10,15 +11,44 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-    Game game;
-    
-    //Player player(Pawn::WHITE);
-    //MinMax minmax(Pawn::BLACK);
+    if (argc < 3)
+    {
+        cout << "Invalid number of arguments" << endl;
+        return 0;
+    }
 
-    Random r1(Pawn::BLACK);
-    Random r2(Pawn::WHITE);
-    
-    game.startGame(r1, r2);
+    AInterface* interface1;
+    AInterface* interface2;
+    if (((string)"player").compare(argv[1]) == 0)
+    {
+        interface1 = new Player(Pawn::BLACK);
+    }
+    else if (((string)"random").compare(argv[1]) == 0)
+    {
+        interface1 = new Random(Pawn::BLACK);
+    }
+    else
+    {
+        cout << "Invalid player 1 argument" << endl;
+        return 0;
+    }
+
+    if (((string)"player").compare(argv[2]) == 0)
+    {
+        interface2 = new Player(Pawn::WHITE);
+    }
+    else if (((string)"random").compare(argv[2]) == 0)
+    {
+        interface2 = new Random(Pawn::WHITE);
+    }
+    else
+    {
+        cout << "Invalid player 2 argument" << endl;
+        return 0;
+    }
+
+    Game game;
+    game.startGame(*interface1, *interface2);
     game.analyseGame(true);
 
     return 0;
